c5854d064e65094820b9d560d715801f6f04788b
Lancea <lanceabuela@gmail.com>
2026-06-08 12:20:06 +0800

    CORS Configuration tweaks

--- src/main/java/com/lancea/studium/studium_api/config/SecurityConfig.java
+++ src/main/java/com/lancea/studium/studium_api/config/SecurityConfig.java
@@ -79,7 +79,7 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource(){
 
         CorsConfiguration corsConfiguration = new CorsConfiguration();
-        corsConfiguration.setAllowedOriginPatterns(List.of("http://localhost:5173/"));
+        corsConfiguration.setAllowedOriginPatterns(List.of("http://localhost:5173"));
         corsConfiguration.setAllowedMethods(List.of("*"));
         corsConfiguration.setAllowedHeaders(List.of("*"));
         corsConfiguration.setAllowCredentials(true);
